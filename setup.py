ac06120abd076d27a7455c041caf54dbf3566cf1
Sathvik <bloodraven66@github.com>
2022-07-24 14:36:42 +0530

    setting up

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='TTS',
+    version='0.0.1',
+    author='Sathvik Udupa',
+    author_email='sathvikudupa66@gmail.com',
+    packages=['TTS'],
+    install_requires=['requests'],
+)
